941db732021d3f6886889bbdb87c5174824623b4
Quentin FORMEY <qformey.dev@gmail.com>
2019-11-28 11:04:22 +0100

    Add files via upload

--- API Open Data/js/EventService.js
+++ API Open Data/js/EventService.js
@@ -1,4 +1,4 @@
-async function getLastEvents() {
+async function getRecentEvents() {
     const data = await $.ajax(`https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?rows=3`);
 
     // Traitement du résultat
@@ -7,12 +7,11 @@ async function getLastEvents() {
     return events;
 }
 
-async function getNextEvents() {
-    const data = await $.ajax(`https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?rows=3&sort=-date_start`);
+async function getAllEvents() {
+    const data = await $.ajax(`https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records`);
 
     // Traitement du résultat
     const events = data.records.map(record => record.record.fields);
     
     return events;
 }
-const url = 'https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?rows=3'
--- API Open Data/js/app.js
+++ API Open Data/js/app.js
@@ -1,38 +1,110 @@
 
-function showLastEvents(events) {
+function showRecentEvents(events) {
     events.forEach(event => {
-        $('#recent-event').append(`<div>
-            <h3>${event.title}</h3>
-            <img src="${event.cover_url}" width="200">
-        </div>`);
+        let excerptDescription = event.lead_text;
+        let excerptTitle = event.title;
+        let excerptDate = event.date_start;
+
+		if (excerptDescription.length > 100){
+			excerptDescription = excerptDescription.slice(0,100) + ' [...]'
+        }
+        if (excerptTitle.length > 16){
+			excerptTitle = excerptTitle.slice(0,16) + ' [...]'
+        }
+        if (excerptDate.length > 10){
+			excerptDate = excerptDate.slice(0,10)
+        }
+        
+
+        $('#recent-event').append(`
+        
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 template-event">
+            <div class="card-all">
+                <div class="vignette-contain">
+                    <img class="vignette" src="${event.cover_url}">
+                </div>
+                <div class="card-content">
+                    <div class="upper-card">
+                        <h3>${excerptTitle}</h3>
+                        <p class="date-added">${excerptDate}</p>
+                    </div>
+                    <div class="in-card">
+                        <p>
+                        ${excerptDescription}
+                        </p>
+
+                        <a class="add-fav" href="#"><img src="asset/fav.png"></a>
+                        <a class="read-more" href="single.html">Voir plus</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `);
     });
 }
 
-function showNextEvents(events) {
+
+function showAllEvents(events) {
     events.forEach(event => {
-        $('#next-events').append(`<div>
-            <h3>${event.title}</h3>
-            <img src="${event.cover_url}" width="200">
-        </div>`);
+        let excerptDescription = event.lead_text;
+        let excerptTitle = event.title;
+        let excerptDate = event.date_start;
+
+		if (excerptDescription.length > 100){
+			excerptDescription = excerptDescription.slice(0,100) + ' [...]'
+        }
+        if (excerptTitle.length > 16){
+			excerptTitle = excerptTitle.slice(0,16) + ' [...]'
+        }
+        if (excerptDate.length > 10){
+			excerptDate = excerptDate.slice(0,10)
+        }
+        
+
+        $('#all-event').append(`
+        
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 template-event">
+            <div class="card-all">
+                <div class="vignette-contain">
+                    <img class="vignette" src="${event.cover_url}">
+                </div>
+                <div class="card-content">
+                    <div class="upper-card">
+                        <h3>${excerptTitle}</h3>
+                        <p class="date-added">${excerptDate}</p>
+                    </div>
+                    <div class="in-card">
+                        <p>
+                        ${excerptDescription}
+                        </p>
+
+                        <a class="add-fav" href="#"><img src="asset/fav.png"></a>
+                        <a class="read-more" href="single.html">Voir plus</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `);
     });
 }
 
-// ================================
-// Avec la méthode async/await
-// ================================
+(async function () {
+    const [RecentEvents] = await Promise.all([getRecentEvents()]);
 
-(async function() {
-    const [lastEvents, nextEvents] = await Promise.all([getLastEvents(), getNextEvents()]);
-
-    showLastEvents(lastEvents);
-    showNextEvents(nextEvents);
+    showRecentEvents(RecentEvents);
 })();
 
-setInterval(function() {
-    $('#timer').text(new Date().getMilliseconds())
-}, 10);
+(async function () {
+    const [AllEvents] = await Promise.all([getAllEvents()]);
 
-   
+    showAllEvents(AllEvents);
+})();
+
+
+
+
 
 
 
